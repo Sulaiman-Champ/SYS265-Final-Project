f86bf4367507a1c8ff07f25d602b6ecf113d0393
Malformed Aluminum Alloy <milo.sanokklis@mymail.champlain.edu>
2026-04-03 17:10:24 -0400

    Add SSH key generation and permission setup
    Generate an SSH key and set permissions for the deployer user.

--- passwordless_auth/keygen.sh
+++ passwordless_auth/keygen.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+# saves the key to the proper location
+ssh-keygen -t id_ed25519.pub -f /home/deployer/.ssh/id_ed25519
+
+# fix all the permissions
 user='deployer'
-
-ssh-keygen -t id_ -C "ARBITRATRY"
-
 chmod 700 ./.ssh
 chmod 600 ./authorized_keys
 sudo chown -R $user:$user /home/$user/.ssh
